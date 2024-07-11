1d63de258cbebd62d45bb876f310659e19532607
Rui Luo <luorui2023@shanghaitech.edu.cn>
2024-07-12 02:24:51 +0800

    add a delay in __init__.py to make sure the server start successfully.

--- packNudft/__init__.py
+++ packNudft/__init__.py
@@ -1,9 +1,11 @@
 import os.path as path
 import subprocess as sp
 import atexit
+import time
 
 dirThisFile, nameThisFile = path.split(__file__)
 process = sp.Popen(path.join(dirThisFile, "NudftServer/build/NudftServer"))
+time.sleep(500e-3) # sleep for a while to let the server start
 
 def cleanup():
     global process
