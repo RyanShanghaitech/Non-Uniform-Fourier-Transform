2509d829acdbd033e135ba33037b89915216ff96
Rui Luo <luorui2023@shanghaitech.edu.cn>
2024-07-17 04:57:46 +0800

    add building commands in install.bash, and modify .gitingore

--- install.bash
+++ install.bash
@@ -1,1 +1,4 @@
 conda install gcc_linux-64 make cmake qt -y
+mkdir ./packNudft/NudftServer/build/
+cmake cmake -S ./packNudft/NudftServer/ -B ./packNudft/NudftServer/build/
+make -C ./packNudft/NudftServer/build/
