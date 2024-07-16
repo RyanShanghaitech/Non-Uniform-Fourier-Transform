56147e31de4f09b578fd3e2d13e87a8ad6a7d988
Rui Luo <luorui2023@shanghaitech.edu.cn>
2024-07-17 05:12:30 +0800

    remove build folder from cache

--- packNudft/NudftServer/build/CMakeFiles/NudftServer_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/NudftServer_autogen"
-  "NudftServer_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/NudftServer_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
