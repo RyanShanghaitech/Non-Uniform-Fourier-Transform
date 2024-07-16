9be9581f5151884b71a96b78faa98d13161b5e37
Rui Luo <luorui2023@shanghaitech.edu.cn>
2024-07-17 06:59:22 +0800

    Restructure and rename

--- nudft/NudftClient.py
+++ nudft/NudftClient.py
@@ -9,45 +9,45 @@ class NudftClient:
         except:
             raise ConnectionError("in classNudftClient")
 
-    def _packData(self, flagIdft:bool, listCoorIn:ndarray, listDataIn:ndarray, listCoorOut:ndarray) -> ndarray:
-        assert(listCoorIn.ndim == 2 and listDataIn.ndim == 1 and listCoorOut.ndim == 2)
-        assert(listCoorIn.shape[0] == listDataIn.shape[0]) # num of point consistency
-        assert(listCoorIn.shape[1] == listCoorOut.shape[1]) # dim consistency
+    def _packData(self, flagIdft:bool, lstCoorIn:ndarray, lstDataIn:ndarray, lstCoorOut:ndarray) -> ndarray:
+        assert(lstCoorIn.ndim == 2 and lstDataIn.ndim == 1 and lstCoorOut.ndim == 2)
+        assert(lstCoorIn.shape[0] == lstDataIn.shape[0]) # num of point consistency
+        assert(lstCoorIn.shape[1] == lstCoorOut.shape[1]) # dim consistency
 
         # derive metadata
-        numDim = uint8(listCoorIn.shape[1])
+        numDim = uint8(lstCoorIn.shape[1])
         typeTf = uint8(1 + 2*(numDim-1)) if flagIdft else uint8(2*(numDim-1))
-        numCoorIn = uint64(listCoorIn.shape[0])
-        numCoorOut = uint64(listCoorOut.shape[0])
-        listCoorIn = listCoorIn.astype(float64).flatten()
-        listDataIn = listDataIn.astype(complex128)
-        listDataIn = array([listDataIn.real, listDataIn.imag], dtype=float64).T.flatten()
-        listCoorOut = listCoorOut.astype(float64).flatten()
+        numCoorIn = uint64(lstCoorIn.shape[0])
+        numCoorOut = uint64(lstCoorOut.shape[0])
+        lstCoorIn = lstCoorIn.astype(float64).flatten()
+        lstDataIn = lstDataIn.astype(complex128)
+        lstDataIn = array([lstDataIn.real, lstDataIn.imag], dtype=float64).T.flatten()
+        lstCoorOut = lstCoorOut.astype(float64).flatten()
 
         # derive validation sum
-        bytesPkgTx = typeTf.tobytes() + numCoorIn.tobytes() + numCoorOut.tobytes() + listCoorIn.tobytes() + listDataIn.tobytes() + listCoorOut.tobytes()
-        listPkgTx = array(list(bytesPkgTx), dtype=uint8)
+        bytesPkgTx = typeTf.tobytes() + numCoorIn.tobytes() + numCoorOut.tobytes() + lstCoorIn.tobytes() + lstDataIn.tobytes() + lstCoorOut.tobytes()
+        lstPkgTx = array(list(bytesPkgTx), dtype=uint8)
 
-        sumBytes = uint8(sum(listPkgTx))
+        sumBytes = uint8(sum(lstPkgTx))
 
         bytesPkgTx += sumBytes.tobytes()
-        listPkgTx = append(listPkgTx, [sumBytes], 0)
+        lstPkgTx = append(lstPkgTx, [sumBytes], 0)
 
         # add header, footer, and escape
-        idx0xFA = where(listPkgTx == 0xFA)[0]
-        idx0xFB = where(listPkgTx == 0xFB)[0]
-        idx0xFC = where(listPkgTx == 0xFC)[0]
+        idx0xFA = where(lstPkgTx == 0xFA)[0]
+        idx0xFB = where(lstPkgTx == 0xFB)[0]
+        idx0xFC = where(lstPkgTx == 0xFC)[0]
 
-        listPkgTx[idx0xFA] = 0xFD
-        listPkgTx[idx0xFB] = 0xFE
-        listPkgTx[idx0xFC] = 0xFF
+        lstPkgTx[idx0xFA] = 0xFD
+        lstPkgTx[idx0xFB] = 0xFE
+        lstPkgTx[idx0xFC] = 0xFF
 
-        listPkgTx = insert(listPkgTx, concatenate((idx0xFA, idx0xFB, idx0xFC), 0), [uint8(0xFB)], 0)
-        listPkgTx = insert(listPkgTx, 0, [uint8(0xFA)], 0)
-        listPkgTx = append(listPkgTx, [uint8(0xFC)], 0)
+        lstPkgTx = insert(lstPkgTx, concatenate((idx0xFA, idx0xFB, idx0xFC), 0), [uint8(0xFB)], 0)
+        lstPkgTx = insert(lstPkgTx, 0, [uint8(0xFA)], 0)
+        lstPkgTx = append(lstPkgTx, [uint8(0xFC)], 0)
 
         # return
-        return listPkgTx
+        return lstPkgTx
     
     def _unpackData(self, bytesPkgRx:bytes) -> ndarray:
         # check header, footer
@@ -55,24 +55,24 @@ class NudftClient:
         assert(bytesPkgRx[-1] == 0xFC)
 
         # convert to array
-        listPkgRx = frombuffer(bytesPkgRx, dtype=uint8).copy()
+        lstPkgRx = frombuffer(bytesPkgRx, dtype=uint8).copy()
 
         # remove header, footer and escape
-        listPkgRx = listPkgRx[1:-1]
-        assert(listPkgRx[-1] != 0xFB)
-        idx0xFB = where(listPkgRx == 0xFB)[0]
+        lstPkgRx = lstPkgRx[1:-1]
+        assert(lstPkgRx[-1] != 0xFB)
+        idx0xFB = where(lstPkgRx == 0xFB)[0]
 
-        listPkgRx[idx0xFB + 1] -= 0x03
-        listPkgRx = delete(listPkgRx, idx0xFB, 0)
+        lstPkgRx[idx0xFB + 1] -= 0x03
+        lstPkgRx = delete(lstPkgRx, idx0xFB, 0)
 
         # check sum
-        assert(uint8(sum(listPkgRx[:-1])) == listPkgRx[-1])
+        assert(uint8(sum(lstPkgRx[:-1])) == lstPkgRx[-1])
 
         # derive data
-        listDataOut = frombuffer(listPkgRx[:-1], dtype=float64)
+        lstDataOut = frombuffer(lstPkgRx[:-1], dtype=float64)
 
         # return
-        return listDataOut
+        return lstDataOut
 
     def _acqPkg(self) -> bytes:
         # self.objSocket.setblocking(False)
@@ -95,23 +95,23 @@ class NudftClient:
                 break
         return bytesPkgRx
     
-    def nudft(self, listX:ndarray, listIx:ndarray, listK:ndarray) -> ndarray:
-        assert(listX.shape[0] == listIx.shape[0]) # num of point consistency
-        assert(listX.shape[1] == listK.shape[1]) # dim consistency
-        listPkgTx = self._packData(False, listX, listIx, listK)
-        self.objSocket.send(listPkgTx)
-        listPkgRx = self._acqPkg()
-        listSk = self._unpackData(listPkgRx)
-        listSk = listSk[0::2] + 1j*listSk[1::2]
-        return listSk
+    def nudft(self, lstX:ndarray, lstIx:ndarray, lstK:ndarray) -> ndarray:
+        assert(lstX.shape[0] == lstIx.shape[0]) # num of point consistency
+        assert(lstX.shape[1] == lstK.shape[1]) # dim consistency
+        lstPkgTx = self._packData(False, lstX, lstIx, lstK)
+        self.objSocket.send(lstPkgTx)
+        lstPkgRx = self._acqPkg()
+        lstSk = self._unpackData(lstPkgRx)
+        lstSk = lstSk[0::2] + 1j*lstSk[1::2]
+        return lstSk
     
-    def nuidft(self, listK:ndarray, listSk:ndarray, listX:ndarray) -> ndarray:
-        assert(listK.shape[0] == listSk.shape[0]) # num of point consistency
-        assert(listK.shape[1] == listX.shape[1]) # dim consistency
-        listPkgTx = self._packData(True, listK, listSk, listX)
-        self.objSocket.send(listPkgTx)
+    def nuidft(self, lstK:ndarray, lstSk:ndarray, lstX:ndarray) -> ndarray:
+        assert(lstK.shape[0] == lstSk.shape[0]) # num of point consistency
+        assert(lstK.shape[1] == lstX.shape[1]) # dim consistency
+        lstPkgTx = self._packData(True, lstK, lstSk, lstX)
+        self.objSocket.send(lstPkgTx)
         bytesPkgRx = self._acqPkg()
-        listIx = self._unpackData(bytesPkgRx)
-        listIx = listIx[0::2] + 1j*listIx[1::2]
-        return listIx
+        lstIx = self._unpackData(bytesPkgRx)
+        lstIx = lstIx[0::2] + 1j*lstIx[1::2]
+        return lstIx
 
