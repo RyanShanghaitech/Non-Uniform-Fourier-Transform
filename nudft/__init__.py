bfb90d5befaff9cb241f4e827a8ee96fc95adc35
Rui Luo <luorui2023@shanghaitech.edu.cn>
2024-08-04 10:01:21 +0800

    improve readability of

--- nudft/__init__.py
+++ nudft/__init__.py
@@ -1,15 +1,12 @@
+from .NudftClient import NudftClient
+
+# register cleanup at exit
 import os.path as path
 import subprocess as sp
 import atexit
 import time
-from .NudftClient import *
 
 dirThisFile, nameThisFile = path.split(__file__)
 process = sp.Popen(path.join(dirThisFile, "NudftServer/build/NudftServer"))
+atexit.register(lambda: process.terminate())
 time.sleep(500e-3) # sleep for a while to let the server start
-
-def cleanup():
-    global process
-    process.terminate()
-
-atexit.register(cleanup)
